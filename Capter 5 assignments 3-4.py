b16875a4e203182038dfbcce4a49f7a6e531c684
ZviMelkman <zvi.melkman@gmail.com>
2020-09-07 10:28:33 +0300

    Chapter 5 Assignment 3, 4

--- Capter 5 assignments 3-4.py
+++ Capter 5 assignments 3-4.py
@@ -10,11 +10,11 @@ Unum = int(input("Please enter an integer. "))
 Vnum = Unum
 if (Unum % 7 == 0):
     print("Boom")
-while (Vnum >= 0):
-    if(Vnum % 10 == 7):
-        print("Boom")
-        break
-    else:
-        b = Vnum / 10
+    while (Vnum >= 0):
+        if(Vnum % 10 == 7):
+            print("Boom")
+            break
+        else:
+            b = Vnum / 10
 else:
     print("You number is not dividable by 7 or contains the digit 7.")
