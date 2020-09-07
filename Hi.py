5c59dacbd1c3773623ed3598b22d2d0638041b9d
ZviMelkman <zvi.melkman@gmail.com>
2020-09-07 09:50:32 +0300

    assignment 1, 2, 3

--- Hi.py
+++ Hi.py
@@ -1,2 +1,11 @@
+# assignment 1:
 Uname = input("Hi, What is your name? ")
 print("Hi, " + Uname)
+
+# assignment 2:
+YearAge = int(input((Uname + " How old are you? ")))
+print("you are " + str(YearAge*12) + " months old.")
+
+# assignment 3:
+Uage = int(input((Uname + " What is your age in months? ")))
+print(Uname + ", You are " + str(Uage/12) + " years old.")
