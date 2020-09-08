8673c395bfc75fd7b3977c8d041c14e10ba596c1
ZviMelkman <zvi.melkman@gmail.com>
2020-09-08 17:43:22 +0300

    Chapter 5 Assignment 6 Corrected

--- Capter 5 Assignment 6.py
+++ Capter 5 Assignment 6.py
@@ -1,9 +1,6 @@
 
-firstNum = int(input("Please enter an integer. "))
+first_num = int(input("Please enter an integer. "))
 difference = int(input("Please enter the difference. "))
-sequence = int(input("Please enter the sequence. "))
-sum = firstNum
+n = int(input("Please enter the sequence. "))
 
-for x in range(0, sequence):
-    sum = sum + difference
-    print(sum)
+print("The total is: " + str((n * (2 * first_num + (n - 1) * difference)) / 2))
