4618bf4e16454372f81958ecfcfebe11355f2a1c
ZviMelkman <zvi.melkman@gmail.com>
2020-09-24 11:25:25 +0300

    Chapter 21 Assignment 1

--- Chap 21 Ex 1.py
+++ Chap 21 Ex 1.py
@@ -1,24 +1,25 @@
 
 class Summer:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
 
-    def add(self, num1, num2):
-        return num1 + num2
+    num1 = 0
+    num2 = 0
+
+    def add(self):
+        return self.num1 + self.num2
 
     def print_total(self):
         print(f'{self.add()}')
 
-# s = Summer()
-# t = Summer()
-#
-# s.add(10, 20)
-# t.add(50)
-# s.add(30)
-#
-# # should print 60
-# s.print_total()
+
+s = Summer()
+t = Summer()
+
+s.add(10, 20)
+t.add(50)
+s.add(30)
+
+# should print 60
+s.print_total()
 
 # should print 50
 t.print_total()
